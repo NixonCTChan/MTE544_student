fb4482d4b81a5788a2256ad565c415f6173749ab
Nixon Chan <68513632+NixonCTChan@users.noreply.github.com>
2024-09-29 23:08:25 -0400

    init lab 1

--- motions.py
+++ motions.py
@@ -1,161 +1,143 @@
 # Imports
 import rclpy
-
 from rclpy.node import Node
-
 from utilities import Logger, euler_from_quaternion
 from rclpy.qos import QoSProfile
 
-# TODO Part 3: Import message types needed: 
-    # For sending velocity commands to the robot: Twist
-    # For the sensors: Imu, LaserScan, and Odometry
-# Check the online documentation to fill in the lines below
-from ... import Twist
-from sensor_msgs.msg import Imu
-from ... import LaserScan
-from ... import Odometry
+# Importing the necessary message types
+from geometry_msgs.msg import Twist  # For sending velocity commands
+from sensor_msgs.msg import Imu, LaserScan  # For the sensors
+from nav_msgs.msg import Odometry  # For Odometry
 
 from rclpy.time import Time
 
-# You may add any other imports you may need/want to use below
-# import ...
+# Additional imports (if needed)
+# import math
 
-
-CIRCLE=0; SPIRAL=1; ACC_LINE=2
-motion_types=['circle', 'spiral', 'line']
+CIRCLE = 0
+SPIRAL = 1
+ACC_LINE = 2
+motion_types = ['circle', 'spiral', 'line']
 
 class motion_executioner(Node):
     
     def __init__(self, motion_type=0):
-        
         super().__init__("motion_types")
         
-        self.type=motion_type
+        self.type = motion_type
+        self.radius_ = 0.0
         
-        self.radius_=0.0
+        self.successful_init = False
+        self.imu_initialized = False
+        self.odom_initialized = False
+        self.laser_initialized = False
         
-        self.successful_init=False
-        self.imu_initialized=False
-        self.odom_initialized=False
-        self.laser_initialized=False
+        # Creating a publisher to send velocity commands
+        self.vel_publisher = self.create_publisher(Twist, 'cmd_vel', 10)
         
-        # TODO Part 3: Create a publisher to send velocity commands by setting the proper parameters in (...)
-        self.vel_publisher=self.create_publisher(...)
-                
-        # loggers
-        self.imu_logger=Logger('imu_content_'+str(motion_types[motion_type])+'.csv', headers=["acc_x", "acc_y", "angular_z", "stamp"])
-        self.odom_logger=Logger('odom_content_'+str(motion_types[motion_type])+'.csv', headers=["x","y","th", "stamp"])
-        self.laser_logger=Logger('laser_content_'+str(motion_types[motion_type])+'.csv', headers=["ranges", "angle_increment", "stamp"])
+        # Loggers
+        self.imu_logger = Logger('imu_content_' + str(motion_types[motion_type]) + '.csv', headers=["acc_x", "acc_y", "angular_z", "stamp"])
+        self.odom_logger = Logger('odom_content_' + str(motion_types[motion_type]) + '.csv', headers=["x", "y", "th", "stamp"])
+        self.laser_logger = Logger('laser_content_' + str(motion_types[motion_type]) + '.csv', headers=["ranges", "angle_increment", "stamp"])
         
-        # TODO Part 3: Create the QoS profile by setting the proper parameters in (...)
-        qos=QoSProfile(...)
-
-        # TODO Part 5: Create below the subscription to the topics corresponding to the respective sensors
-        # IMU subscription
+        # Creating QoS profile
+        qos = QoSProfile(depth=10)
         
-        ...
+        # Subscriptions to sensor topics
+        self.create_subscription(Imu, 'imu', self.imu_callback, qos)
+        self.create_subscription(Odometry, 'odom', self.odom_callback, qos)
+        self.create_subscription(LaserScan, 'scan', self.laser_callback, qos)
         
-        # ENCODER subscription
-
-        ...
-        
-        # LaserScan subscription 
-        
-        ...
-        
+        # Create a timer that calls the timer_callback every 0.1 seconds
         self.create_timer(0.1, self.timer_callback)
 
-
-    # TODO Part 5: Callback functions: complete the callback functions of the three sensors to log the proper data.
-    # To also log the time you need to use the rclpy Time class, each ros msg will come with a header, and then
-    # inside the header you have a stamp that has the time in seconds and nanoseconds, you should log it in nanoseconds as 
-    # such: Time.from_msg(imu_msg.header.stamp).nanoseconds
-    # You can save the needed fields into a list, and pass the list to the log_values function in utilities.py
-
     def imu_callback(self, imu_msg: Imu):
-        ...    # log imu msgs
-        
+        # Log IMU messages
+        acc_x = imu_msg.linear_acceleration.x
+        acc_y = imu_msg.linear_acceleration.y
+        angular_z = imu_msg.angular_velocity.z
+        stamp = Time.from_msg(imu_msg.header.stamp).nanoseconds
+        self.imu_logger.log_values([acc_x, acc_y, angular_z, stamp])
+        self.imu_initialized = True
+
     def odom_callback(self, odom_msg: Odometry):
-        
-        ... # log odom msgs
-                
+        # Log odometry messages
+        x = odom_msg.pose.pose.position.x
+        y = odom_msg.pose.pose.position.y
+        orientation = odom_msg.pose.pose.orientation
+        _, _, th = euler_from_quaternion(orientation)
+        stamp = Time.from_msg(odom_msg.header.stamp).nanoseconds
+        self.odom_logger.log_values([x, y, th, stamp])
+        self.odom_initialized = True
+
     def laser_callback(self, laser_msg: LaserScan):
-        
-        ... # log laser msgs with position msg at that time
-                
+        # Log laser scan messages
+        ranges = laser_msg.ranges
+        angle_increment = laser_msg.angle_increment
+        stamp = Time.from_msg(laser_msg.header.stamp).nanoseconds
+        self.laser_logger.log_values([ranges, angle_increment, stamp])
+        self.laser_initialized = True
+
     def timer_callback(self):
-        
         if self.odom_initialized and self.laser_initialized and self.imu_initialized:
-            self.successful_init=True
+            self.successful_init = True
             
         if not self.successful_init:
             return
         
-        cmd_vel_msg=Twist()
+        cmd_vel_msg = Twist()
         
-        if self.type==CIRCLE:
-            cmd_vel_msg=self.make_circular_twist()
+        if self.type == CIRCLE:
+            cmd_vel_msg = self.make_circular_twist()
         
-        elif self.type==SPIRAL:
-            cmd_vel_msg=self.make_spiral_twist()
+        elif self.type == SPIRAL:
+            cmd_vel_msg = self.make_spiral_twist()
                         
-        elif self.type==ACC_LINE:
-            cmd_vel_msg=self.make_acc_line_twist()
+        elif self.type == ACC_LINE:
+            cmd_vel_msg = self.make_acc_line_twist()
             
         else:
             print("type not set successfully, 0: CIRCLE 1: SPIRAL and 2: ACCELERATED LINE")
             raise SystemExit 
 
         self.vel_publisher.publish(cmd_vel_msg)
-        
-    
-    # TODO Part 4: Motion functions: complete the functions to generate the proper messages corresponding to the desired motions of the robot
 
     def make_circular_twist(self):
-        
-        msg=Twist()
-        ... # fill up the twist msg for circular motion
+        msg = Twist()
+        msg.linear.x = 0.2  # Constant forward speed
+        msg.angular.z = 0.2  # Constant angular speed for circular motion
         return msg
 
     def make_spiral_twist(self):
-        msg=Twist()
-        ... # fill up the twist msg for spiral motion
+        self.radius_ += 0.01  # Increase radius for spiral motion
+        msg = Twist()
+        msg.linear.x = self.radius_ * 0.2  # Forward speed proportional to radius
+        msg.angular.z = 0.2  # Constant angular speed
         return msg
-    
+
     def make_acc_line_twist(self):
-        msg=Twist()
-        ... # fill up the twist msg for line motion
+        msg = Twist()
+        msg.linear.x = 0.5  # Constant acceleration in a straight line
         return msg
 
 import argparse
 
-if __name__=="__main__":
-    
-
-    argParser=argparse.ArgumentParser(description="input the motion type")
-
-
+if __name__ == "__main__":
+    argParser = argparse.ArgumentParser(description="input the motion type")
     argParser.add_argument("--motion", type=str, default="circle")
-
-
-
+    
     rclpy.init()
 
     args = argParser.parse_args()
 
     if args.motion.lower() == "circle":
-
-        ME=motion_executioner(motion_type=CIRCLE)
+        ME = motion_executioner(motion_type=CIRCLE)
     elif args.motion.lower() == "line":
-        ME=motion_executioner(motion_type=ACC_LINE)
-
-    elif args.motion.lower() =="spiral":
-        ME=motion_executioner(motion_type=SPIRAL)
-
+        ME = motion_executioner(motion_type=ACC_LINE)
+    elif args.motion.lower() == "spiral":
+        ME = motion_executioner(motion_type=SPIRAL)
     else:
-        print(f"we don't have {arg.motion.lower()} motion type")
-
-
+        print(f"we don't have {args.motion.lower()} motion type")
     
     try:
         rclpy.spin(ME)
--- utilities.py
+++ utilities.py
@@ -1,91 +1,98 @@
 from math import atan2, asin, sqrt
 
-M_PI=3.1415926535
+M_PI = 3.1415926535
 
 class Logger:
     def __init__(self, filename, headers=["e", "e_dot", "e_int", "stamp"]):
         self.filename = filename
 
         with open(self.filename, 'w') as file:
-            header_str=""
+            header_str = ""
 
             for header in headers:
-                header_str+=header
-                header_str+=", "
+                header_str += header
+                header_str += ", "
             
-            header_str+="\n"
-            
+            header_str += "\n"
             file.write(header_str)
 
-
     def log_values(self, values_list):
-
         with open(self.filename, 'a') as file:
-            vals_str=""
+            vals_str = ""
 
-            # TODO Part 5: Write the values from the list to the file
-            ...
+            # Write the values from the list to the file
+            for val in values_list:
+                vals_str += str(val) + ", "
             
-            vals_str+="\n"
-            
+            vals_str += "\n"
             file.write(vals_str)
-            
 
     def save_log(self):
         pass
 
 class FileReader:
     def __init__(self, filename):
-        
         self.filename = filename
-        
-        
-    def read_file(self):
-        
-        read_headers=False
 
-        table=[]
-        headers=[]
+    def read_file(self):
+        read_headers = False
+        table = []
+        headers = []
+
         with open(self.filename, 'r') as file:
             # Skip the header line
-
             if not read_headers:
                 for line in file:
-                    values=line.strip().split(',')
+                    values = line.strip().split(',')
 
                     for val in values:
-                        if val=='':
+                        if val == '':
                             break
                         headers.append(val.strip())
 
-                    read_headers=True
+                    read_headers = True
                     break
-            
-            next(file)
-            
+
+            next(file)  # Skip the first line
+
             # Read each line and extract values
             for line in file:
                 values = line.strip().split(',')
-                
-                row=[]                
-                
+                row = []
+
                 for val in values:
-                    if val=='':
+                    if val == '':
                         break
                     row.append(float(val.strip()))
 
                 table.append(row)
-        
+
         return headers, table
 
 
-# TODO Part 5: Implement the conversion from Quaternion to Euler Angles
+# Conversion from Quaternion to Euler Angles
 def euler_from_quaternion(quat):
     """
     Convert quaternion (w in last place) to euler roll, pitch, yaw.
     quat = [x, y, z, w]
     """
-    ... # just unpack yaw
-    return yaw
+    x, y, z, w = quat
 
+    # Roll (x-axis rotation)
+    sinr_cosp = 2 * (w * x + y * z)
+    cosr_cosp = 1 - 2 * (x * x + y * y)
+    roll = atan2(sinr_cosp, cosr_cosp)
 
+    # Pitch (y-axis rotation)
+    sinp = 2 * (w * y - z * x)
+    if abs(sinp) >= 1:
+        pitch = M_PI / 2 * (sinp / abs(sinp))  # Use 90 degrees if out of range
+    else:
+        pitch = asin(sinp)
+
+    # Yaw (z-axis rotation)
+    siny_cosp = 2 * (w * z + x * y)
+    cosy_cosp = 1 - 2 * (y * y + z * z)
+    yaw = atan2(siny_cosp, cosy_cosp)
+
+    return roll, pitch, yaw
