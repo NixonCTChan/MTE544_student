5ac970a8de57173e8eb8fc507f9d31d648fea148
Nixon Chan <68513632+NixonCTChan@users.noreply.github.com>
2024-10-04 00:01:51 -0400

    fix plotting

--- filePlotter.py
+++ filePlotter.py
@@ -3,6 +3,7 @@
 # Feel free to make any modifications/additions here
 
 import matplotlib.pyplot as plt
+import numpy as np
 from utilities import FileReader
 
 def plot_errors(filename):
@@ -11,17 +12,35 @@ def plot_errors(filename):
     time_list=[]
     first_stamp=values[0][-1]
     
-    for val in values:
-        time_list.append(val[-1] - first_stamp)
+    if "laser" in filename:
+        content_headers, content_values=FileReader(filename.replace("laser_ranges_content", "laser_content")).read_file() 
+        ranges_headers, ranges_values=FileReader(filename.replace("laser_content", "laser_ranges_content")).read_file()
+        time_list=[]
+        cartesian_lasers=[]
+        first_stamp=content_values[0][-1]
+        angle = content_values[0][0]
+        angles = np.linspace(0, angle * len(ranges_values[0][0]), len(ranges_values[0][0]))
+        angles = np.array([angles])
+        for val in zip(content_values, ranges_values):
+            time_list.append(val[0][1] - first_stamp)
+            ranges = np.array(val[1])
+            mask = ranges != np.inf
+            ranges = ranges[mask]
+            cartesian_lasers.append([ranges*np.cos(angles[mask]), ranges*np.sin(angles[mask])])
+        for val in cartesian_lasers:
+            plt.scatter(val[0], val[1], s=0.5) 
+        plt.grid()
+        plt.show()   
+    else:
+        for val in values:
+            time_list.append(val[-1] - first_stamp)
 
-    for i in range(0, len(headers) - 1):
-        plt.plot(time_list, [lin[i] for lin in values], label= headers[i]+ " linear")
-    
-    #plt.plot([lin[0] for lin in values], [lin[1] for lin in values])
-    plt.legend()
-    plt.grid()
-    plt.show()
-    
+        for i in range(0, len(headers) - 1):
+            plt.plot(time_list, [lin[i] for lin in values], label= headers[i]+ " linear")
+        plt.legend()
+        plt.grid()
+        plt.show()
+        
 import argparse
 
 if __name__=="__main__":
